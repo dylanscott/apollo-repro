476d75a4e8e5e090ddbfb44959865944b808ea47
Dylan Scott <dylan.scott@gmail.com>
2017-09-21 11:56:43 -0700

    upgrade to beta apollo-client

--- src/index.js
+++ src/index.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { ApolloProvider, ApolloClient, createNetworkInterface } from 'react-apollo';
+import { ApolloProvider } from 'react-apollo';
+import ApolloClient from 'apollo-client';
+import Link from 'apollo-link-http';
+import Cache from 'apollo-cache-inmemory';
+
 import App from './App';
 import './index.css';
 
 const client = new ApolloClient({
-  networkInterface: createNetworkInterface({
-    uri: '/graphql',
-  }),
-  connectToDevTools: true,
-})
+  link: new Link({ uri: '/graphql' }),
+  cache: new Cache(),
+});
 
 ReactDOM.render(
   <ApolloProvider client={client}>
